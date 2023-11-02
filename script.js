2332e81b0de28db03700487d85b9745cb75a645d
sawaizmirza <m_sawaiz1791@ymail.com>
2023-11-02 19:31:17 +0000

    added dark mode and burger icon to mobile version

--- script.js
+++ script.js
@@ -8,4 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
             project.style.transform = 'scale(1)';
         });
     });
+
+    // Burger menu functionality
+    const burgerMenu = document.getElementById('burgerMenu');
+    const navLinks = document.querySelector('.nav-links');
+
+    burgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+
+    // Dark mode toggle functionality
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    // Check for saved user preference
+    const darkMode = localStorage.getItem('darkMode');
+    
+    // Set initial mode
+    if (darkMode === 'enabled') {
+        body.classList.add('dark-mode');
+        darkModeToggle.textContent = '☀️';
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            darkModeToggle.textContent = '☀️';
+        } else {
+            localStorage.setItem('darkMode', null);
+            darkModeToggle.textContent = '🌙';
+        }
+    });
 });
